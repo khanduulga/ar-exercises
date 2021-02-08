5255e1ba58b540fa62f7d8533bed552ef1dcde06
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2021-02-08 02:48:13 +0000

    First exercise complete

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,45 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+burnaby = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true,
+)
+
+richmond = Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true,
+)
+
+gastown = Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false,
+)
+
+puts "THERE ARE #{Store.count()} stores in the DB."
+
+# ActiveRecord::Schema.define do
+#   drop_table :stores if ActiveRecord::Base.connection.table_exists?(:stores)
+#   drop_table :employees if ActiveRecord::Base.connection.table_exists?(:employees)
+#   create_table :stores do |t|
+#     t.column :name, :string
+#     t.column :annual_revenue, :integer
+#     t.column :mens_apparel, :boolean
+#     t.column :womens_apparel, :boolean
+#     t.timestamps null: false
+#   end
+#   create_table :employees do |table|
+#     table.references :store
+#     table.column :first_name, :string
+#     table.column :last_name, :string
+#     table.column :hourly_rate, :integer
+#     table.timestamps null: false
+#   end
+# end
