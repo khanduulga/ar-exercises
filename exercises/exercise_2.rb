b51057fb8bb4ad5f9270bac67c6ee3e2fa80fef4
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2021-02-08 02:52:00 +0000

    Found stores using id and updated name for first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Vancouver"
+@store1.save
+
+#or @store1.update(name: "Vancouver")
+
+puts @store1.name
+puts @store2.name
