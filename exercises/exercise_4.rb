61863db51c0f9321f6fab36662bfb9378d00bef2
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2021-02-08 03:00:47 +0000

    ex3 finished using destroy method

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,30 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Surrey = Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true,
+)
+
+whistler = Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false,
+)
+
+yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true,
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+puts @mens_stores.name
+# .each |store| do
+#   puts store.name
+#   # puts store.annual_revenue
+# end
